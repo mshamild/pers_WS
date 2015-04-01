81e65180a88fefa1554ade28686aaf2f811c075e
mshamild <mshd91@gmail.com>
2015-04-02 00:00:56 +0300

    russian interfeis

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,8 @@ require 'github/markdown'
 
 class DashboardController < ApplicationController
   expose(:content) do
-    GitHub::Markdown.render(File.read("#{Rails.root}/README.md"))
+  	# Рендеринг readme файла:
+    # GitHub::Markdown.render(File.read("#{Rails.root}/README.md"))
   end
 
   def index
